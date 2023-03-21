93ecb18a206f20dd127fa6dcba2c1b7cd93e26a4
Bruno Oliveira <brunocosta1699320@gmail.com>
2023-03-20 21:13:56 -0300

    1

---
+++ viacep/README.md
@@ -0,0 +1,21 @@
+Readme - API de Consulta de CEP
+Este é um programa em PHP que utiliza a API do ViaCEP para realizar consultas de CEP.
+
+
+Para utilizar a API de consulta de CEP, execute o arquivo index.php no terminal.
+
+Copy code
+php index.php
+Será solicitado que informe o CEP que deseja consultar.
+
+yaml
+Digite o CEP que deseja consultar:
+Após digitar o CEP e pressionar a tecla Enter, o programa irá realizar a consulta na API do ViaCEP e exibirá as informações do endereço correspondente.
+
+Caso ocorra algum erro na consulta, será exibida uma mensagem de erro.
+
+Personalização
+Você pode personalizar a exibição das informações do endereço alterando o método printAddress na classe Address.
+
+Licença
+Este programa é distribuído sob a Licença MIT. Consulte o arquivo LICENSE para obter mais informações.
--- viacep/front.php
+++ viacep/front.php
@@ -1,58 +1,81 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Pesquisa de CEP</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
-<body>
-    <form method="post">
-        <input type="text" name="cep" placeholder="cep">
-        <br>
-        <button type="submit" name="submit" value="submit">Calcular</button>
-    </form>
-    <fieldset>
-        <legend>Resultado</legend>
-        
-        <?php
-        if(isset($_POST['submit'])) {
-            $cep = $_POST['cep'];
-            $url = "https://viacep.com.br/ws/" . $cep . "/json/";
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($ch);
-            curl_close($ch);
-            if ($response) {
-                $address = json_decode($response);
-                if (isset($address->erro)) {
-                    echo "Erro: CEP não encontrado.\n";
-                } else {
-                    echo "Endereço:\n";
-                    echo "CEP: " . $address->cep . "\n";
-                    echo "Logradouro: " . $address->logradouro . "\n";
-                    echo "Complemento: " . $address->complemento . "\n";
-                    echo "Bairro: " . $address->bairro . "\n";
-                    echo "Cidade: " . $address->localidade . "\n";
-                    echo "Estado: " . $address->uf . "\n";
-                }
-            } else {
-                echo "Erro na consulta.\n";
-            }
-        }
-        ?>
-        
-    </fieldset>
+
+<body class="container bodysuperior">
+    <fieldset class="container field_1">
+
+        <form class="container" method="post">
+            <div class="seach_bar">
+                <input class="input_search" type="text" name="cep" placeholder="cep">
+                <button class="button-seach-cep" type="submit" name="submit" value="submit">Calcular</button>
+            </div>
+            <br>
+        </form>
 </body>
+<div class="container div_1">
+    <fieldset class="container field-resultado">
+        <legend class="leg_fiel">Resultado</legend>
+       
+        <br>
+        <br>
+
+        <!-- Codigo abaixo que possibilita a busca de CEP
+    ------------------------------------------------------------- -->
+        <p class="p_fiel">
+            <?php
+            if (isset($_POST['submit'])) {
+                $cep = $_POST['cep'];
+                $url = "https://viacep.com.br/ws/" . $cep . "/json/";
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($ch);
+                curl_close($ch);
+                if ($response) {
+                    $address = json_decode($response);
+                    if (isset($address->erro)) {
+                        echo "Erro: CEP não encontrado.\n";
+                    } else {
+                        echo "Endereço:\n";
+                        echo "CEP: " . $address->cep . "\n";
+                        echo "Logradouro: " . $address->logradouro . "\n";
+                        echo "Complemento: " . $address->complemento . "\n";
+                        echo "Bairro: " . $address->bairro . "\n";
+                        echo "Cidade: " . $address->localidade . "\n";
+                        echo "Estado: " . $address->uf . "\n";
+                    }
+                } else {
+                    echo "Erro na consulta.\n";
+                }
+            }
+            ?>
+
+        </p>
+    </fieldset>
+
+    </fieldset>
+</div>
+
+
+
+<br>
+</body>
+
 </html>
 
-<?php 
+<?php
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 //Dependencias da api 
 use \App\WebService\ViaCEP;
 
 //Pega o que foi digitado no comando do terminal
-if(!isset($argv[1])){
+if (!isset($argv[1])) {
     die("CEP não definido\n");
 }
 
@@ -63,4 +86,3 @@ $dadosCEP = ViaCEP::consultarCEP($argv[1]);
 print_r($dadosCEP);
 
 ?>
-
